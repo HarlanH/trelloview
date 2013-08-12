b07c4bec1b6c30b65353d6df564f594675bfa36d
Harlan Harris <harlan@harris.name>
2013-08-12 00:06:16 +0000

    add mustache and simple html output for dsdc.html

--- web.js
+++ web.js
@@ -8,7 +8,10 @@ var Trello = require("node-trello");
 var public_key = process.env.TRELLO_KEY;
 var trello_token = process.env.TRELLO_TOKEN; // TODO: replace with stored auth from admin user
 var t = new Trello(public_key, trello_token);
+
 var _ = require("underscore");
+var fs = require('fs');
+var mustache = require('mustache'); 
 
 var get_dsdc = function(callback) {
     t.get("/1/boards/4ffd9be44795a71d1101b2bc", 
@@ -27,24 +30,23 @@ var get_dsdc = function(callback) {
 
 app.get('/dsdc', function(req, res){
   // get JSON for DSDC events and send back
-  //res.send(get_dsdc());
   console.log("Got request for DSDC");
   get_dsdc(res.send.bind(res));
-  //res.send("asdf");
-//    t.get("/1/boards/4ffd9be44795a71d1101b2bc", //?cards=open&card_fields=name,due,labels,idList,pos&fields=name,desc&lists=open", 
-//        { cards: "open", card_fields: ["name", "due", "labels", "idList", "pos"], fields: ["name", "desc"], lists: "open"},
-//        function(err, data) {
-//        if (err) throw err;
-//        
-//        private_id = _.findWhere(data.lists, {name: "Private"}).id;
-//
-//        data.cards = _.filter(data.cards, function(card) { return card.idList != private_id; });
-//        res.send(data);
-//        
-//    });
 });
 
-// TODO: dsdc.html that generates a nice table
+// generates a nice table
+var make_table = function(callback, data) {
+    
+    // convert the object to html here
+    var rData = {records: data.cards}; 
+    var page = fs.readFileSync("dsdc.html", "utf8"); // bring in the HTML file
+    var html = mustache.to_html(page, rData);
+    callback(html);
+};
+
+app.get('/dsdc.html', function(req,res) {
+    get_dsdc(_.partial(make_table, res.send.bind(res)));
+});
 
 app.get('/sentrana', function(req, res){
     // get JSON for Sentrana cards, build a table, and send back.
